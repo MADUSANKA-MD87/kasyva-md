33e5c9cf7e0dd629d3bb534373b9407c30b2a006
MADUSANKA-MD87 <kasunnisakara99@gmail.com>
2026-05-05 19:21:10 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const BOT_NAME_FANCY = '✦ ━━ MADUSANKA-MD ━━ ✦';
+
+const config = {
+  AUTO_VIEW_STATUS: 'true',
+  AUTO_LIKE_STATUS: 'true',
+  AUTO_RECORDING: 'false',
+  AUTO_LIKE_EMOJI: ['☘️','💗','🫂','🙈','🍁','🙃','🧸','😘','🏴‍☠️','👀','❤️‍🔥'],
+  PREFIX: '.',
+  MAX_RETRIES: 3,
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/I7eReF73VGu4EU9kF9w4uS',
+  IMAGE_PATH: 'https://i.ibb.co/pB4Y9PrD/c30081ff2ffb.jpg',
+  NEWSLETTER_JID: '120363423916773660@newsletter',
+  OTP_EXPIRY: 300000,
+  WORK_TYPE: 'public',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '94783731694',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb7p3UCCHDyocfEGm23f',
+  BOT_NAME: '✦ ━━ MADUSANKA-MD ━━ ✦',
+  BOT_VERSION: '2.0.0V',
+  OWNER_NAME: 'DAMITH MADUSANKA',
+  IMAGE_PATH: 'https://i.ibb.co/pB4Y9PrD/c30081ff2ffb.jpg',
+  BOT_FOOTER: '> *©ᴘᴏᴡᴇʀᴅ ʙʏ ⚔️ |      MADUSANKA-MD|| 🐉*',
+  BUTTON_IMAGES: { ALIVE: 'https://i.ibb.co/pB4Y9PrD/c30081ff2ffb.jpg' }
+};
